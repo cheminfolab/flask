a2f7897a9b162217d086c11b88877a32e5a62a64
cheminfolab <janha@protonmail.com>
2022-07-21 13:01:14 +0200

    updated CORS_ALLOW_ALL_ORIGINS .env specification

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -200,6 +200,6 @@ MEDIA_URL = 'images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=False)
+CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=False, cast=bool)
 # CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='127.0.0.1', cast=Csv())
 
