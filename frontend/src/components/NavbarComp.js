ac739caabbc36ded6911b7cc562e975d3f629ff5
cheminfolab <janha@protonmail.com>
2022-07-24 18:15:08 +0200

    updated NavbarComp.js

--- frontend/src/components/NavbarComp.js
+++ frontend/src/components/NavbarComp.js
@@ -1,5 +1,15 @@
 import React, {useContext} from 'react';
-import { Container, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import {
+    Container,
+    Navbar,
+    Nav,
+    NavDropdown,
+    Form,
+    FormControl,
+    Button,
+    DropdownButton,
+    Dropdown
+} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 import AuthContext from "../context/AuthContext";
@@ -22,17 +32,14 @@ const NavbarComp = () => {
                         <NavDropdown.Item as={Link} to={"/chemicals"}>Chemicals</NavDropdown.Item>
                         <NavDropdown.Item as={Link} to={"/#"}>Equipment</NavDropdown.Item>
                         <NavDropdown.Item as={Link} to={"/#"}>Something</NavDropdown.Item>
+
                         <NavDropdown.Divider />
+
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                     <Nav.Link as={Link} to={"/"}>Equipment</Nav.Link>
-                    {user ? (
-                        <Nav.Link onClick={logoutUser}>Logout</Nav.Link>
-                    ):(
-                        <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>
-                    )}
                   </Nav>
-                  <Form className="d-flex">
+                  <Form className="d-flex mx-auto">
                     <FormControl
                       type="search"
                       placeholder="Search"
@@ -41,6 +48,24 @@ const NavbarComp = () => {
                     />
                     <Button variant="outline-success">Search</Button>
                   </Form>
+                  <Nav className="mr-auto">
+                      <DropdownButton
+                        id={`dropdown-variants-secondary`}
+                        variant={'secondary'}
+                        title={'Settings'}
+                      >
+                        <Dropdown.Item eventKey="1">Account</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+
+                        <Dropdown.Divider />
+
+                        {user ? (
+                            <Dropdown.Item onClick={logoutUser}>Logout</Dropdown.Item>
+                        ):(
+                            <Dropdown.Item as={Link} to={"/login"}>Login</Dropdown.Item>
+                        )}
+                      </DropdownButton>
+                  </Nav>
                 </Navbar.Collapse>
               </Container>
             </Navbar>
