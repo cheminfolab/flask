8e789cded41ceabc6fdbab9be45ebfb30cfbf563
cheminfolab <janha@protonmail.com>
2022-07-24 23:18:33 +0200

    minor modifications

--- frontend/src/components/SubstanceImage.js
+++ frontend/src/components/SubstanceImage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './SubstanceImage.css'
 
 const SubstanceImage = ({path}) => {
-    console.log('http://127.0.0.1:8000'+String(path))
     return (
         <div>
             <img src={'http://127.0.0.1:8000'+String(path)} alt={'chemical structure'}/>
