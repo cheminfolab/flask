740505bb2c5a24f354b7e3d46370ff5cb2eb7500
cheminfolab <janha@protonmail.com>
2022-08-03 00:28:12 +0200

    updated sidebar and detailed compound component

--- frontend/src/pages/ChemPage.js
+++ frontend/src/pages/ChemPage.js
@@ -10,6 +10,8 @@ const ChemPage = () => {
 
     let {logoutUser} = useContext(AuthContext)
     let [compounds, setCompounds] = useState([])
+    let [compoundDetail, setCompoundDetail] = useState([])
+    let [compoundId, setCompoundId] = useState(1)
     let [show, setShow] = useState(false)
     let api = useAxios(true)
 
@@ -17,11 +19,11 @@ const ChemPage = () => {
         getCompounds();
     }, [])
 
-    let getCompounds = async () => {
-        await api.get('/compound/')
+    let getapi = async (url, setState) => {
+        await api.get(url)
             .then( (response) => {
                 if (response.status === 200){
-                    setCompounds(response.data)
+                    setState(response.data)
                 } else if (response.statusText === 'Unauthorized'){
                     logoutUser()
                 }
@@ -29,15 +31,33 @@ const ChemPage = () => {
             .catch( (error) => console.log(error))
     }
 
+    let getCompounds = () => getapi('/compound/', setCompounds)
+    let getCompoundDetail = id => getapi(`/compound/${id}`, setCompoundDetail)
+
+    useEffect(() => {
+        if (compoundId){
+            getCompoundDetail(compoundId)
+        }
+        return
+    }, [compoundId])
+
     return (
         <Container fluid>
-            <ChemDetailComp show={show} setShow={setShow}/>
             <Row>
                 <Col xs={2} id="sidebar-wrapper">
                     <ChemSidebarComp/>
                 </Col>
                 <Col  xs={10} id="page-content-wrapper">
-                    <ChemTableComp compounds={compounds} setShow={setShow}/>
+                    <ChemTableComp
+                        compounds={compounds}
+                        setShow={setShow}
+                        setCompoundId={setCompoundId}
+                    />
+                    <ChemDetailComp
+                        show={show}
+                        setShow={setShow}
+                        compoundDetail={compoundDetail}
+                    />
                 </Col>
             </Row>
         </Container>
