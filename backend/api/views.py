72dccbf4f614f2069d7a6c251d7cf7c13b425712
cheminfolab <janha@protonmail.com>
2023-07-21 00:41:06 +0200

    fixed bug in POST (create) method

--- backend/api/views.py
+++ backend/api/views.py
@@ -44,7 +44,8 @@ class CustomViewSet(ViewSet):
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            self.model.objects.create_user(**serializer.validated_data)
+            # todo: problems with create user?
+            self.model.objects.create(**serializer.validated_data)
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
